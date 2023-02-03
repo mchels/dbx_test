a84874b4b47eaabcb0c423947b6d35d0940c0cc7
Morten Hels <umrh@novonordisk.com>
2023-02-03 13:39:02 +0100

    Import pandas as well

--- dbx-demo-job.py
+++ dbx-demo-job.py
@@ -1,3 +1,7 @@
+import pandas as pd
+
 print("hello from the dbx side")
-print("I have called jeg ved ikke hvor mange gange!")
-1+1
+print("I have called, like, seriously SO many times!")
+
+df = pd.DataFrame([1, 2, 3])
+print(df)
